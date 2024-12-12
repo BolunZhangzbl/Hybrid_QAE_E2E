e63118c133b0ef05d30b570db26249381575e37a
Bolun Zhang <13580850696@163.com>
2024-12-12 23:20:43 +0000

    12/12/2024; add more instances in test_runtime.py

--- test_runtime.py
+++ test_runtime.py
@@ -1,16 +1,13 @@
 # -- Public Imports
-import gc
-import os
-import random
 import pennylane as qml
 from pennylane import numpy as np
 
 import timeit
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
-import tensorflow.keras.backend as K
 from tensorflow.keras.layers import Input, Lambda, Add, Dense, Activation, Reshape, Flatten
-
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 # --Private Imports
 
@@ -34,7 +31,7 @@ def get_onehot_data(data_size, bit_num):
     return onehot_data
 
 
-def create_qml_circuit(dev, num_qubits, bit_num):
+def create_qml_circuit(dev, num_qubits, bit_num, interface):
     def layer1(layer_weights):
         """
         q layer for fading:
@@ -46,7 +43,7 @@ def create_qml_circuit(dev, num_qubits, bit_num):
         for wire in range(0, num_qubits - 1):
             qml.CNOT(wires=[wire, (wire + 1)])
 
-    @qml.qnode(dev, diff_method='best', mutable=False)
+    @qml.qnode(dev, interface=interface, diff_method='best')
     def qcircuit_complex(inputs, weights):
 
         qml.AmplitudeEmbedding(inputs, wires=range(bit_num), normalize=True)
@@ -63,33 +60,73 @@ def transmitter(num_qubits, bit_num):
     ipl = Input((2**bit_num,))
     d1 = Dense(2**bit_num, activation='relu')(ipl)
     d2 = Dense(num_qubits, activation='linear')(d1)
-    nl = Lambda(lambda x: tf.sqrt(tf.cast(num_qubits, tf.float32)) * (K.l2_normalize(x, axis=[1])))(d2)
+    # nl = Lambda(lambda x: tf.sqrt(tf.cast(num_qubits, tf.float32)) * (K.l2_normalize(x, axis=[1])))(d2)
 
-    model = tf.keras.models.Model(inputs=ipl, outputs=nl)
+    model = tf.keras.models.Model(inputs=ipl, outputs=d2)
 
     return model
 
 
 def time_inference(iters=1000):
-    dev = qml.device("default.qubit", wires=4)
-    weight_init = np.random.rand(3, 1, 4, 1)
-    qml_circuit = create_qml_circuit(dev, 4, 4)
+    dev_default = qml.device("default.qubit", wires=4)
+    dev_lightning = qml.device("lightning.gpu", wires=4)
+    weight_qml = np.random.rand(3, 1, 4, 1) * 0.01
+    qml_circuit_default0 = create_qml_circuit(dev_default, 4, 4, 'tensorflow')
+    qml_circuit_default1 = create_qml_circuit(dev_default, 4, 4, None)
+    qml_circuit_lightning0 = create_qml_circuit(dev_lightning, 4, 4, 'tensorflow')
+    qml_circuit_lightning1 = create_qml_circuit(dev_lightning, 4, 4, None)
+
+    weight_tfq = np.random.rand(3, 1, 4, 1)
+    # tfq_circuit = create_tfq_circuit(num_qubits=4, bit_num=4)
+
+    weight_qiskit = np.random.rand(3, 1, 4, 1)
+
     dnn = transmitter(4, 4)
-    # tfq_circuit = create_tfq_circuit(4, 4)
 
-    data = get_onehot_data(32, 4)
+    data = get_onehot_data(1, 4)[0]
+    # data = np.random.rand(1024, 4)
 
-    # Pennylane Circuit Timing
-    print(f"0. Calculating avg running time for Q Circuit using Pennylane......")
-    execution_time = timeit.timeit(lambda: qml_circuit(data, weight_init), number=iters)
+    # Pennylane Circuit Timing - Default
+    print(f"\n0. Calculating avg running time for Q Circuit using Pennylane - Default......")
+    execution_time = timeit.timeit(lambda: qml_circuit_default0(data, weight_qml), number=iters)
     avg_time0 = execution_time / iters
-    print(f"0. Finish calculating avg running time for Q Circuit using Pennylane - {avg_time0} sec.")
+    print(f"0. Finish calculating avg running time for Q Circuit using Pennylane - Default - {avg_time0} sec.")
+
+    # Pennylane Circuit Timing - Default
+    print(f"\n1. Calculating avg running time for Q Circuit using Pennylane - Default......")
+    execution_time = timeit.timeit(lambda: qml_circuit_default1(data, weight_qml), number=iters)
+    avg_time1 = execution_time / iters
+    print(f"1. Finish calculating avg running time for Q Circuit using Pennylane - Default - {avg_time1} sec.")
+
+    # Pennylane Circuit Timing - Lightning
+    print(f"\n2. Calculating avg running time for Q Circuit using Pennylane - Lightning......")
+    execution_time = timeit.timeit(lambda: qml_circuit_lightning0(data, weight_qml), number=iters)
+    avg_time2 = execution_time / iters
+    print(f"2. Finish calculating avg running time for Q Circuit using Pennylane - Lightning - {avg_time2} sec.")
+
+    # Pennylane Circuit Timing - Lightning
+    print(f"\n3. Calculating avg running time for Q Circuit using Pennylane - Lightning......")
+    execution_time = timeit.timeit(lambda: qml_circuit_lightning1(data, weight_qml), number=iters)
+    avg_time3 = execution_time / iters
+    print(f"3. Finish calculating avg running time for Q Circuit using Pennylane - Lightning - {avg_time3} sec.")
 
     # DNN Timing
-    print(f"1. Calculating avg running time for DNN......")
-    execution_time = timeit.timeit(lambda: dnn(data), number=iters)
-    avg_time1 = execution_time / iters
-    print(f"1. Finish calculating avg running time for DNN - {avg_time1} sec.")
+    print(f"\n4. Calculating avg running time for DNN......")
+    execution_time = timeit.timeit(lambda: dnn(np.expand_dims(data, axis=0)), number=iters)
+    avg_time4 = execution_time / iters
+    print(f"4. Finish calculating avg running time for DNN - {avg_time4} sec.")
+
+    # Generating the bar plot for avg_time comparisons
+    methods = ['Pennylane (default, tf)', 'Pennylane (default, None)',
+               'Pennylane (lightning, tf)', 'Pennylane (lightning, None)',
+               'Tensorflow']
+    avg_times = [avg_time0, avg_time1, avg_time2, avg_time3, avg_time4]
+
+    plt.figure(figsize=(15, 10))
+    plt.bar(methods, avg_times, color=['blue', 'green', 'red', 'orange', 'purple'])
+    plt.ylabel('Average Time (sec)')
+    plt.title('Average Running Time Comparison for Different Quantum and DNN Methods')
+    plt.show()
 
 
-# time_inference()
+time_inference()
